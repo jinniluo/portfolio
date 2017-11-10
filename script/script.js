9ec9c2bbfb3513c2bcef4e40466138635fcce843
Kini Luo <kini1993129@gmail.com>
2017-11-09 16:15:49 -0800

    create portfolio

---
+++ script/script.js
@@ -0,0 +1,14 @@
+console.log("run")
+
+//hover and click
+$("a").hover(function(){$(this).toggleClass( "hover" );});
+//
+// $("img").hover(function(){$(this).toggleId( "toggle" );})
+// var h=$(".content_img_group1").height();
+// $(".content_img").height(h);
+
+// var h2=$(".content_img_group2").height();
+// var h2=h2*3+20*2;
+// $(".content_img2").height(h2);
+
+
